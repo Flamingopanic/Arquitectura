f505b2f04f1eaeea9bf201449b7f555770580963
Flamingopanic <163334358+Flamingopanic@users.noreply.github.com>
2024-09-21 21:12:43 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
+
+# Define el directorio donde Django buscará tus archivos estáticos en desarrollo
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp', 'static')]
+
+# Define el directorio donde se guardarán los archivos estáticos una vez que ejecutes collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
